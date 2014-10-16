10560a20ee353ad17910c16d8cc4eeeca1c88612
nian <linian365boy@foxmail.com>
2014-10-16 20:23:11 +0800

    some  dao commit

--- src/main/java/com/waimai/controller/DishController.java
+++ src/main/java/com/waimai/controller/DishController.java
@@ -1,9 +1,15 @@
 package com.waimai.controller;  
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
 /**
  * 
  * @author linian
  *
  */
+@Controller
+@Scope("prototype")
 public class DishController {
 
 }
