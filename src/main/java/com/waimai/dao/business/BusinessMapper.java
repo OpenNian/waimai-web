9eb119f3cad111c3ad73ea83627c1bc1bca92d4e
=huxiaolong <764562057@qq.com>
2014-11-08 12:46:30 +0800

    修改对应的字段信息，及busines的mapper

--- src/main/java/com/waimai/dao/business/BusinessMapper.java
+++ src/main/java/com/waimai/dao/business/BusinessMapper.java
@@ -1,5 +1,24 @@
 package com.waimai.dao.business;  
 
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import com.waimai.model.business.Business;
+
 public interface BusinessMapper {
+	/**
+	 * @FunName: findParentMenuByRole
+	 * @Description:  根据所属角色查找能访问的一级菜单
+	 * @param roles
+	 * @return
+	 * @Author: 胡小龙
+	 * @CreateDate: 2013-3-28
+	 */
+	public List<Business> findBusiness(Collection<Business> Business);
+
+	public Business findOne(Serializable id);
 	
+	public void delete(Serializable id);
+	
+	public Business save(Business bus);
 }
